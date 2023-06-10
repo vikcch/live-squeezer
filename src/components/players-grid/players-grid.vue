9c27212f082ae530fe41abfc1580bf183deef6d5
vik <vikcch@hotmail.com>
2023-06-10 19:43:00 +0100

    refactor: sitout

--- src/components/players-grid/players-grid.vue
+++ src/components/players-grid/players-grid.vue
@@ -77,8 +77,8 @@ import { tail } from '../../units/absx';
 import biz from '../../units/biz';
 import Player from '../../classes/player.js';
 import validation from '../../units/validations';
+import sitoutEase from '../../eases/sitout';
 
-import { makeTextTag } from '../../units/fxnl.js';
 
 const autoFillerData = [
 	{ seat: '1', name: 'vik', stack: '2000', holeCards: '__ __' },
@@ -138,13 +138,6 @@ const orderCardsDisplay = function () {
 	});
 };
 
-
-const sitouttersHandler = function () {
-
-	this.value = validation.force.onlyNumbers(this.value);
-};
-
-
 export default {
 	components: {
 		'app-player-row': PlayerRow
@@ -170,60 +163,9 @@ export default {
 		enable,
 		orderCardsDisplay,
 
-		async sitout() {
+		sitout() {
 
-			const { view } = this.$root.$data;
-			const tableMax = view.mainInfoVue.$data.values.tableMax;
-
-			const limit = biz.playersLimitTableMax(tableMax);
-
-			if (this.inputs.length >= limit) return this.$swal.fire({ title: 'Table is full' });
-
-			const askForSeating = async (sitouts) => {
-
-				const markTag = makeTextTag('mark');
-
-				const sitouttersSeat = sitouts.map(v => ` ${markTag(v.seat)}`);
-
-				const htmlText = `Players on Sitout: ${sitouttersSeat}`;
-
-				const result = await this.$swal.fire({
-					html: htmlText,
-					input: 'text',
-					onBeforeOpen: (saEl) => {
-						const input = saEl.querySelector('.swal2-input');
-						input.setAttribute('inputmode', 'numeric');
-						input.addEventListener('input', sitouttersHandler);
-					},
-					onClose: (saEl) => {
-						const input = saEl.querySelector('.swal2-input');
-						input.removeEventListener('input', sitouttersHandler);
-					}
-				});
-
-				const foundSitoutter = sitouts.find(v => v.seat === result?.value);
-
-				if (result.value && !foundSitoutter) return await askForSeating();
-
-				return foundSitoutter;
-			};
-
-			const { sitouts } = this.$data;
-
-			const sitoutter = sitouts.length > 1
-				? await askForSeating(sitouts)
-				: sitouts[0];
-
-			if (!sitoutter) return;
-
-			const { seat, name, stack } = sitoutter;
-			this.inputs.push(Player.model(seat, name, stack));
-			this.$data.sitouts = sitouts.filter(v => v !== sitoutter);
-
-			const uniquesSeats = new Set(this.inputs.map(v => v.seat)).size;
-			if (uniquesSeats !== this.inputs.length) {
-				this.$swal.fire({ title: 'Duplicate Seats!' });
-			}
+			sitoutEase.call(this);
 		},
 
 		getAllHoleCardsEl() {
---
+++ src/eases/sitout.js
@@ -0,0 +1,78 @@
+
+import Vue from 'vue';
+import biz from '../units/biz';
+import validation from '../units/validations';
+import { makeTextTag } from '../units/fxnl.js';
+import Player from '../classes/player.js';
+
+const sitouttersHandler = function () {
+
+    this.value = validation.force.onlyNumbers(this.value);
+};
+const askForSeating = async (sitouts) => {
+
+    const markTag = makeTextTag('mark');
+
+    const sitouttersSeat = sitouts.map(v => ` ${markTag(v.seat)}`);
+
+    const htmlText = `Players on Sitout: ${sitouttersSeat}`;
+
+    const result = await Vue.swal.fire({
+        html: htmlText,
+        input: 'text',
+        onBeforeOpen: (saEl) => {
+            const input = saEl.querySelector('.swal2-input');
+            input.setAttribute('inputmode', 'numeric');
+            input.addEventListener('input', sitouttersHandler);
+        },
+        onClose: (saEl) => {
+            const input = saEl.querySelector('.swal2-input');
+            input.removeEventListener('input', sitouttersHandler);
+        }
+    });
+
+    const foundSitoutter = sitouts.find(v => v.seat === result?.value);
+
+    if (result.value && !foundSitoutter) return await askForSeating();
+
+    return foundSitoutter;
+};
+
+const fullTable = function () {
+
+    const { view } = this.$root.$data;
+    const tableMax = view.mainInfoVue.$data.values.tableMax;
+
+    const limit = biz.playersLimitTableMax(tableMax);
+
+    if (this.inputs.length >= limit) return this.$swal.fire({ title: 'Table is full' });
+};
+
+const checkDuplicateSeats = function () {
+
+    const uniquesSeats = new Set(this.inputs.map(v => v.seat)).size;
+
+    if (uniquesSeats !== this.inputs.length) {
+        
+        this.$swal.fire({ title: 'Duplicate Seats!' });
+    }
+};
+
+export default async function () {
+
+    if (fullTable.call(this)) return;
+
+    const { sitouts } = this.$data;
+
+    const sitoutter = sitouts.length > 1
+        ? await askForSeating(sitouts)
+        : sitouts[0];
+
+    if (!sitoutter) return;
+
+    const { seat, name, stack } = sitoutter;
+    this.inputs.push(Player.model(seat, name, stack));
+    this.$data.sitouts = sitouts.filter(v => v !== sitoutter);
+
+    checkDuplicateSeats.call(this);
+}
